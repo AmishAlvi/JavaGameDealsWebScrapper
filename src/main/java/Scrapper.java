01ee50fc61a5d14d64a76e7d3c95a7bd8ace9521
Amish alvi <amish.alvi@gmail.com>
2020-02-04 17:56:15 +0300

    successfully scrapping for price and item name but not successfully parsing the correct name, parses the same name of the first item over and over

--- src/main/java/Scrapper.java
+++ src/main/java/Scrapper.java
@@ -2,6 +2,8 @@ import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.WebRequest;
 import com.gargoylesoftware.htmlunit.WebResponse;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.util.WebConnectionWrapper;
 import org.jsoup.Jsoup;
@@ -33,9 +35,31 @@ public class Scrapper {
             }
         };
 
-        client.getPage(baseURL);
+        HtmlPage page = client.getPage(baseURL);
         client.waitForBackgroundJavaScript(10_000);
-        list.forEach(System.out::println);
+        //list.forEach(System.out::println);
+
+        //System.out.println(page.asXml());
+
+        List<HtmlElement> items = page.getByXPath("//div[@class='hit-card faux-block-link card']") ;
+        if(items.isEmpty()){
+            System.out.println("No items found !");
+        }else{
+            for(HtmlElement item : items){
+                System.out.println(item.getTagName());
+                HtmlAnchor itemAnchor = ((HtmlAnchor) item.getFirstByXPath("//a[@class='d-flex btn btn-primary']"));
+
+                HtmlElement spanPrice = ((HtmlElement) item.getFirstByXPath("//span[@class='card-price price']")) ;
+
+                String itemName = itemAnchor.asText();
+                String itemUrl =  itemAnchor.getHrefAttribute();
+
+                // It is possible that an item doesn't have any price
+                String itemPrice = spanPrice == null ? "0.0" : spanPrice.asText() ;
+
+                System.out.println( String.format("Name : %s Url : %s Price : %s", itemName, itemPrice, itemUrl));
+            }
+        }
 
 
 
