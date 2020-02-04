6bb288f4a18183bf1f0cbafa97c4ed520f520556
Amish alvi <amish.alvi@gmail.com>
2020-02-04 17:41:55 +0300

    reverting to HTMLunit and parsing all js requests generated by the URL

--- src/main/java/Scrapper.java
+++ src/main/java/Scrapper.java
@@ -1,33 +1,42 @@
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebRequest;
+import com.gargoylesoftware.htmlunit.WebResponse;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.util.WebConnectionWrapper;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Scrapper {
 
-    private static final String baseURL = "https://www.fanatical.com/en/";
+    private static final String baseURL = "https://www.fanatical.com/en/on-sale?onSale=true";
 
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws IOException {
+        WebClient client = new WebClient(BrowserVersion.FIREFOX_60);//creating webclient
+        client.getOptions().setCssEnabled(false);
+        client.getOptions().setThrowExceptionOnScriptError(false);
 
-        try {
-            Document doc = Jsoup.connect(baseURL).get();
+        final List<String> list = new ArrayList<>();
 
-            System.out.printf("Title: %s", doc.title());
-
-            Elements repos = doc.getElementsByClass("col-12");
-
-            if(repos.isEmpty())
-            {
-                System.out.println("ıs empter");
+        new WebConnectionWrapper(client) {
+            @Override
+            public WebResponse getResponse(final WebRequest request) throws IOException {
+                final WebResponse response = super.getResponse(request);
+                list.add(request.getHttpMethod() + " " + request.getUrl());
+                return response;
             }
+        };
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        client.getPage(baseURL);
+        client.waitForBackgroundJavaScript(10_000);
+        list.forEach(System.out::println);
+
 
 
     }
